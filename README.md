630a5cbffbf481e86eae4ad5eac4ef5d2c517b38
kevings <kevings@users.noreply.github.com>
2022-06-29 17:09:16 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+Created for the coding challenge presented here: https://github.com/RiplApp/spellchecker-homework
+
+To set up the local server, I used Python's [SimpleHTTPServer](https://docs.python.org/2/library/simplehttpserver.html) by navigating to the folder directory and running `python -m SimpleHTTPServer 1337` then setting my Chrome browser to http://localhost:1337/index.html. Per the information on [this page](https://gist.github.com/jgravois/5e73b56fa7756fd00b89#what-if-id-rather-use-something-else), it might be different if you are using a different version of Python. I am using Python version 2.7.15.
+
+Once the web server is up and running, you should be able to type in a word in the text field and click Submit to spell check with the following parameters:
+
+- Fixes bad casing (“wetumpka” → “Wetumpka”)
+- Removes repeating letters (“tabble” → “table”)
+- Adds missing repeating letters (“realy” → “really”)
+- Removes all spaces (“ immense" → "immense")
+- All of the above (“pa RNAssussss ” → “Parnassus”)
---
+++ script.js
@@ -0,0 +1,72 @@
+import { dictionary } from "./data.js";
+
+// get form by tag
+const form = document.getElementsByTagName("form")[0];
+
+// event listener to run our function when user hits Submit
+form.addEventListener("submit", function (event) {
+  let word = document.getElementById("wordToCheck").value;
+  document.getElementById(
+    "spellChecked"
+  ).innerHTML = `The correct spelling is: ${checkWord(word)}`;
+  // prevents the Submit button from refreshing page
+  event.preventDefault();
+});
+
+const checkWord = (wordToCheck) => {
+  // create maps and sets
+  const originSet = new Set(dictionary);
+  const lowerCaseMap = new Map();
+  const noRepeatLettersMap = new Map();
+
+  for (const word of dictionary) {
+    // create map of dictionary with all lowercase letters
+    const lowered = word.toLowerCase();
+    if (!lowerCaseMap.has(lowered)) {
+      lowerCaseMap.set(lowered, word);
+    }
+
+    // create map of dictionary with no repeating letters and all lowercase letters
+    const noRepeatLetters = (lowered) => {
+      let result = "";
+      for (let i = 0; i < lowered.length; i++) {
+        if (lowered[i] != lowered[i + 1]) {
+          result += lowered[i];
+        }
+      }
+      return result;
+    };
+    if (!noRepeatLettersMap.has(noRepeatLetters(lowered))) {
+      noRepeatLettersMap.set(noRepeatLetters(lowered), word);
+    }
+  }
+
+  // remove extra spaces from word
+  wordToCheck = wordToCheck.replace(/\s/g, "");
+
+  // check if incoming word is in the dictionary set
+  if (originSet.has(wordToCheck)) return wordToCheck;
+
+  // check if incoming word is in our lowercase map
+  const lowered = wordToCheck.toLowerCase();
+  if (lowerCaseMap.has(lowered)) {
+    return lowerCaseMap.get(lowered);
+  }
+
+  // check if incoming word has is in our non repeating letters map
+  const noRepeatLetters = (lowered) => {
+    let result = "";
+    for (let i = 0; i < lowered.length; i++) {
+      if (lowered[i] != lowered[i + 1]) {
+        result += lowered[i];
+      }
+    }
+    return result;
+  };
+  if (noRepeatLettersMap.has(noRepeatLetters(lowered))) {
+    return noRepeatLettersMap.get(noRepeatLetters(lowered));
+  }
+
+  // if incoming word is not in dictionary and cannot find the correction
+  return "No correction found";
+};
